3cf71745447c5ffe450925c35322e41572cbe9c4
Dardrilkael <gabrielfonsecabarreto@gmail.com>
2025-05-08 12:47:53 -0300

    feat: add health check data publishing to MQTT

--- index/index.ino
+++ index/index.ino
@@ -252,7 +252,11 @@ void loop() {
       if(healthCheck.isMqttConnected)
         mqqtClient1.subscribe((String("sys") + String(config.mqtt_topic)).c_str());
     }
-  
+    // Enviando dados de health check
+    char mqttHealthCheck[100];
+    sprintf(mqttHealthCheck, "{\"wifiDBM\":\"%i\"}", WiFi.RSSI());
+    mqqtClient1.publish((sysReportMqqtTopic + String("/healthcheck")).c_str(), mqttHealthCheck, 1);
+    
    // Atualizando BLE advertsting value
     BLE::updateValue(HEALTH_CHECK_UUID, ("HC: " + String(hcCsv)).c_str());
   }
